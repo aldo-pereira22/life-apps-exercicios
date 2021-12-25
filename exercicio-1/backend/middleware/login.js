5426a284e5ec87c058d32f530834aa01ead16586
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-25 02:59:09 -0300

    Protegendo as rotas com o metodo para identificar o token

--- exercicio-1/backend/middleware/login.js
+++ exercicio-1/backend/middleware/login.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
 
     try {
-        console.log("AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIi")
-        const decode = jwt.verify(req.body.token,"secreto")
+
+        const token = req.headers.authorization.split(' ')[1]
+        console.log("AQYUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU token: "+ token)
+        
+        const decode = jwt.verify(token,"secreto")
         req.usuario = decode
         next()
     } catch (error) {
