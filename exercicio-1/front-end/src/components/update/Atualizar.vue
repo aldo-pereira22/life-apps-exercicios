a695e6b78587161c0c1cd7bea3354fa9e966324f
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-27 10:24:01 -0300

    Buscando o artigo especifico pelo ID

--- exercicio-1/front-end/src/components/update/Atualizar.vue
+++ exercicio-1/front-end/src/components/update/Atualizar.vue
@@ -2,15 +2,26 @@
     <div class="main">
         <h2>Atualizar Artigo</h2>
         <h3>ID: {{id}}</h3>
+        {{artigos}}
     </div>
 </template>
 
 <script>
+import { mapState, mapActions} from 'vuex'
 export default {
     data(){
         return {
             id:this.$route.params.id
         }
+    },
+    computed:{
+        ...mapState(['artigos'])
+    },
+    methods: {
+        ...mapActions(['buscarArtigo'])
+    },
+    created(){
+        this.buscarArtigo(this.$route.params.id)
     }
 
 }
